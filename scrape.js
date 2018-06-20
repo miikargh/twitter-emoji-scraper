68f892bae53327e5917abdc17b3dd3d498c52bd6
miikargh <mamoilanen@gmail.com>
2018-06-20 18:35:31 +0300

    Added support for saving tweets to multiple files

--- scrape.js
+++ scrape.js
@@ -3,34 +3,33 @@ const Twitter = require('twitter')
 const creds = require('./secrets.json')
 const emojiFile = './emojis.txt'
 const tweetFile = './tweets.txt'
-const tweetJSONFile = './tweet_json.txt'
+let lineCounter = 0
+let fileCounter = 0
+const maxFileLineCount = 10
 
 const client = new Twitter(creds)
 const emojis = fs.readFileSync(emojiFile, 'utf8')
 const params = {
     track: emojis,
-    language: 'fi',
-}
-const saveTweetToFile = (filename, tweet) => {
-    const noBrakes = tweet.replace(/\n/gm,'')
-    fs.appendFileSync(filename, noBrakes + '\n')
+    language: 'ar',
 }
 
-const saveTweetObjectToFile = (filename, tweetObj) => {
+const saveTweetObjectToFile = (tweetObj) => {
     const tweetJSON = JSON.stringify(tweetObj)
-    fs.appendFileSync(filename, tweetJSON + '\n')
+
+    if (lineCounter >= maxFileLineCount) {
+        lineCounter = 0
+        fileCounter += 1
+    }
+
+    fs.appendFileSync('./data/tweets_'+ fileCounter +'.txt', tweetJSON + '\n')
+    lineCounter++
 }
 
 client.stream('statuses/filter.json', params, stream => {
 
     stream.on('data', e => {
-        // if (e && e.text) {
-        //     saveTweetToFile(
-        //         tweetFile,
-        //         e.extended_tweet ? e.extended_tweet.full_text : e.text
-        //     )
-        // }
-        saveTweetObjectToFile(tweetJSONFile, e)
+        saveTweetObjectToFile(e)
     })
 
     stream.on('error', err => {
