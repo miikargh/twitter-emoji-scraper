88816e28e11aacaa5b35b75aa952db04e623852b
miikargh <mamoilanen@gmail.com>
2018-06-23 11:35:52 +0300

    Linecount saved to file

--- scrape.js
+++ scrape.js
@@ -3,9 +3,9 @@ const Twitter = require('twitter')
 const creds = require('./secrets.json')
 const emojiFile = './emojis.txt'
 const tweetFile = './tweets.txt'
-let lineCounter = 0
+const lineCounterFile = './current_line_count.txt'
 let fileCounter = 0
-const maxFileLineCount = 9999
+const maxFileLineCount = 10000
 
 const client = new Twitter(creds)
 const emojis = fs.readFileSync(emojiFile, 'utf8')
@@ -14,8 +14,12 @@ const params = {
     language: 'fi',
 }
 
+let lineCounter = Number(fs.readFileSync(lineCounterFile))
+
 const saveTweetObjectToFile = (tweetObj) => {
     const tweetJSON = JSON.stringify(tweetObj)
+
+    console.log('Line counter:', lineCounter)
 
     if (lineCounter >= maxFileLineCount) {
         lineCounter = 0
@@ -23,6 +27,8 @@ const saveTweetObjectToFile = (tweetObj) => {
     }
 
     fs.appendFileSync('./data/tweets_'+ fileCounter +'.txt', tweetJSON + '\n')
+    fs.writeFileSync(lineCounterFile, lineCounter.toString())
+
     lineCounter++
 }
 
